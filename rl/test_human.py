a10f26d6e909a27d11a204d23181545ac24fdab8
Thibault Neveu <thibo73800@gmail.com>
2019-02-14 12:15:02 +0100

    Grid world + test_human file

---
+++ rl/test_human.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+from __future__ import division, print_function
+
+import sys
+import numpy
+import gym
+import time
+from optparse import OptionParser
+
+import gym_minigrid
+
+def main():
+    # Load the gym environment
+    env = gym.make("MiniGrid-Empty-6x6-v0")
+
+    env.reset()
+    renderer = env.render('human')
+
+    def keyDownCb(keyName):
+        if keyName == 'BACKSPACE':
+            resetEnv()
+            return
+
+        if keyName == 'ESCAPE':
+            sys.exit(0)
+
+        action = 0
+
+        if keyName == 'LEFT':
+            action = env.actions.left
+        elif keyName == 'RIGHT':
+            action = env.actions.right
+        elif keyName == 'UP':
+            action = env.actions.forward
+
+        elif keyName == 'SPACE':
+            action = env.actions.toggle
+        elif keyName == 'PAGE_UP':
+            action = env.actions.pickup
+        elif keyName == 'PAGE_DOWN':
+            action = env.actions.drop
+
+        elif keyName == 'RETURN':
+            action = env.actions.done
+
+        else:
+            print("unknown key %s" % keyName)
+            return
+
+        obs, reward, done, info = env.step(action)
+
+        print('step=%s, reward=%.2f' % (env.step_count, reward))
+
+        if done:
+            print('done!')
+            resetEnv()
+
+    renderer.window.setKeyDownCb(keyDownCb)
+
+    while True:
+        env.render('human')
+        time.sleep(0.01)
+
+        # If the window was closed
+        if renderer.window == None:
+            break
+
+if __name__ == "__main__":
+    main()
